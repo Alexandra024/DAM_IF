f366fa9ef460785e22e9553c0beffa15e5f811ee
Alexandra024 <1a.barsan.sara@gmail.com>
2025-04-30 16:10:37 +0200

    pequeño avance mystring

--- Exercises/Ex_TM3_Prg_JAVA/src/MyString.java
+++ Exercises/Ex_TM3_Prg_JAVA/src/MyString.java
@@ -1,74 +1,102 @@
 package Ex_TM3_Prg_JAVA.src;
-// Crear una Clase que funcione imite a la clase String
-// que tendrá como atributo un array de char
 
-import java.util.*;
+//Crear una Clase que funcione imite a la clase String
+//que tendrá como atributo un array de char
+
+import java.util.*; // Importa las librerias publicas 
 
 public class MyString {
     private char[] cadena;
-
-    public MyString(String cadena) {
+    
+    //Crear un MyString a partir de un String de Java
+    public MyString(String s) {
+        this.cadena = new char[s.length()];
+        for (int i = 0; i < s.length(); i++ ){
+            this.cadena[i] = s.charAt(i);
+        }
     }
-
+    
+    // Crear un MyString a partir de un array de caracteres
     public MyString(char[] cadena) {
+
+    }
+    
+    public MyString(ArrayList<Character> cadena){
+
+    }
+    
+    public char[] getCadena(){
+
+    }
+    
+    public char myCharAt(int i) throws Exception{
+
     }
 
-    public MyString(ArrayList<Character> cadena) {
-    }
+    public int myCompareTo(MyString cadena2){
 
-    public char[] getCadena() {
     }
+    
+    public int myCompareToIgnoreCase(MyString cadena2){
 
-    public char myCharAt(int i) throws Exception {
     }
+    
+    public void myConcat(MyString cadena2){
 
-    public int myCompareTo(MyString cadena2) {
     }
+    
+    public int myIndexOf(int caracter){
 
-    public int myCompareToIgnoreCase(MyString cadena2) {
     }
+    
+    public int myLastIndexOf(int caracter){
 
-    public void myConcat(MyString cadena2) {
     }
+    
+    public int myLength(){
 
-    public int myIndexOf(int caracter) {
     }
+    
+    public MyString myReplace(char antiguo, char nuevo){
 
-    public int myLastIndexOf(int caracter) {
     }
-
-    public int myLength() {
-    }
-
-    public MyString myReplace(char antiguo, char nuevo) {
-    }
-
+    
     /*
-     * public MyString[] mySplit(char expresion) {
-     * }
-     */
-
-    public MyString[] mySplit(char expresion) {
+     * public MyString[] mySplit(char expresion)
+    {
     }
+    */
+    
+    public MyString[] mySplit(char expresion){
 
-    public MyString mySubstring(int inicio, int fin) throws Exception {
     }
+        
+    
+    
+    public MyString mySubstring(int inicio, int fin) throws Exception{
 
-    public MyString myToLowerCase() {
     }
+    public MyString myToLowerCase(){
 
-    public MyString myToLowerCase(char[] cadena) {
     }
+    
+    public MyString myToLowerCase(char[] cadena){
 
-    public MyString myToUpperCase() {
     }
+    
+    public MyString myToUpperCase(){
 
-    public MyString myTrim() {
     }
+    
+    public MyString myTrim(){
 
-    public void imprimir() {
     }
+    
+    public void imprimir(){
 
-    public String myToString() {
+    }
+    
+    public String myToString(){
+
     }
 }

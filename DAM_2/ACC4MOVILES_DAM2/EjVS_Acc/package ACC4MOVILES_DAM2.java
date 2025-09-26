2a803882609a1ed96ac208987c0c170f9e841bea
Alexandra024 <1a.barsan.sara@gmail.com>
2025-09-26 08:15:12 +0200

    adjuntar recursos de clase acceso a datos

---
+++ DAM_2/ACC4MOVILES_DAM2/EjVS_Acc/package ACC4MOVILES_DAM2.java
@@ -0,0 +1,16 @@
+package ACC4MOVILES_DAM2.Ejercicios_acc;
+
+public class ExcepcionesDivPorCero {
+    
+    public static void mail(){
+        int a , b;
+        a= 5;
+        b= 2;
+        System.out.println(a + "/" + b + "/" + a/b);
+        b= 0;
+        System.out.println(a + "/" + b + "/" + a/b);
+
+    }
+}
+
+// java.test.simpledateformat
